ebe89709ffc642795c8146d150b2fd8242bab9c8
Ehsan Allahi <ehsanallahi47@gmail.com>
2025-09-15 12:46:39 +0500

    remove unused route matchers from middleware configuration

--- middleware.js
+++ middleware.js
@@ -3,9 +3,5 @@ export { default } from "next-auth/middleware";
 export const config = {
   matcher: [
     '/dashboard',
-    '/orders/:path*',
-    '/customers/:path*',
-    '/settings/:path*',
-    '/track-customer/:path*',
   ],
 };
